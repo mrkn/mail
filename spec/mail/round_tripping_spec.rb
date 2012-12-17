79258a7972248e81633b45a4fef0b7924588c843
Kenta Murata <mrkn@cookpad.com>
2012-12-17 16:36:12 +0900

    Add examples for trailing whitespaces

--- spec/mail/round_tripping_spec.rb
+++ spec/mail/round_tripping_spec.rb
@@ -11,6 +11,15 @@ describe "Round Tripping" do
     parsed_mail.body.to_s.should eq "This is Text"
   end
 
+  it "should round trip a basic email with trailing whitespaces", :focus => true do
+    mail = Mail.new('Subject: FooBar')
+    mail.body "This is Text\n   \n"
+    parsed_mail = Mail.new(mail.to_s)
+    parsed_mail.subject.to_s.should eq "FooBar"
+    parsed_mail.body.to_s.should eq "This is Text\n   \n"
+    parsed_mail.encoded.should eq(mail.encoded)
+  end
+
   it "should round trip a html multipart email" do
     mail = Mail.new('Subject: FooBar')
     mail.text_part = Mail::Part.new do
@@ -26,6 +35,24 @@ describe "Round Tripping" do
     parsed_mail.parts.length.should eq 2
     parsed_mail.parts[0].body.to_s.should eq "This is Text"
     parsed_mail.parts[1].body.to_s.should eq "<b>This is HTML</b>"
+  end
+
+  it "should round trip a html multipart email with trailing whitespaces", :focus => true do
+    mail = Mail.new('Subject: FooBar')
+    mail.text_part = Mail::Part.new do
+      body "This is Text\n   \n"
+    end
+    mail.html_part = Mail::Part.new do
+      content_type "text/html; charset=US-ASCII"
+      body "<b>This is HTML</b>"
+    end
+    parsed_mail = Mail.new(mail.to_s)
+    parsed_mail.mime_type.should eq 'multipart/alternative'
+    parsed_mail.boundary.should eq mail.boundary
+    parsed_mail.parts.length.should eq 2
+    #parsed_mail.parts[0].body.to_s.should eq "This is Text\n   \n"
+    parsed_mail.parts[1].body.to_s.should eq "<b>This is HTML</b>"
+    parsed_mail.encoded.should eq(mail.encoded)
   end
 
   it "should round trip an email" do
